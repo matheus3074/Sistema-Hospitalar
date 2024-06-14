8f8d6a9da19ecd33151bac1a7e596a1dbbed1320
matheus3074 <matheus.ratto11@gmail.com>
2024-06-14 11:52:28 -0300

    Integração

---
+++ funções/evolucao_paciente.c
@@ -0,0 +1,45 @@
+#include "evolucao_paciente.h"
+#include <stdio.h>
+
+void documentoDeEvolucaoDoPaciente() {
+    FILE *arquivoDaEvolucaoDoPaciente; // Declaração de ponteiro de arquivo
+    char nomePaciente[100], nomeMedico[100], idade[20], crm[20], diagnostico[1000], leito[5], sexo[10], diaMesAno[10];
+
+    arquivoDaEvolucaoDoPaciente = fopen("Evolucao_do_paciente.txt", "w");
+
+    if (arquivoDaEvolucaoDoPaciente == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return;
+    } else {
+        // Solicitando informações ao usuário
+        printf("Digite o nome do paciente: ");
+        fgets(nomePaciente, sizeof(nomePaciente), stdin);
+
+        printf("Digite o gênero do paciente: ");
+        fgets(sexo, sizeof(sexo), stdin);
+
+        printf("Digite o nome do médico responsável: ");
+        fgets(nomeMedico, sizeof(nomeMedico), stdin);
+
+        printf("Digite o CRM do médico: ");
+        fgets(crm, sizeof(crm), stdin);
+
+        printf("Escreva o diagnostico do paciente: ");
+        fgets(diagnostico, sizeof(diagnostico), stdin);
+
+        printf("Digite a data da requisição: ");
+        fgets(diaMesAno, sizeof(diaMesAno), stdin);
+
+        fprintf(arquivoDaEvolucaoDoPaciente, "--- Evolução do paciente ---\n");
+
+        fprintf(arquivoDaEvolucaoDoPaciente, "Paciente: %sIdade: %sLeito: %sSexo: %s", nomePaciente, idade, leito, sexo);
+
+        fprintf(arquivoDaEvolucaoDoPaciente, "Médico Responsável: %sCRM: %sDiagnostico: %sData da Requisição: %s", nomeMedico, crm, diagnostico, diaMesAno);
+
+        fprintf(arquivoDaEvolucaoDoPaciente, "--- Fim do Documento ---");
+
+        fclose(arquivoDaEvolucaoDoPaciente);
+
+        printf("Documento de evolução do paciente criado com sucesso!\n");
+    }
+}
---
+++ funções/recepcao_hospital.h
@@ -0,0 +1,28 @@
+#ifndef RECEPCAO_HOSPITAL_H
+#define RECEPCAO_HOSPITAL_H
+
+#define MAX_PACIENTES 200
+#define MAX_COMPRIMENTO_NOME 100
+#define MAX_COMPRIMENTO_SINTOMAS 200
+#define MAX_COMPRIMENTO_CPF 15
+#define MAX_COMPRIMENTO_TELEFONE 20
+#define MAX_COMPRIMENTO_RESIDENCIA 200
+
+typedef struct {
+    char nome[MAX_COMPRIMENTO_NOME];
+    int idade;
+    char cpf[MAX_COMPRIMENTO_CPF];
+    char telefone[MAX_COMPRIMENTO_TELEFONE];
+    char residencia[MAX_COMPRIMENTO_RESIDENCIA];
+    char sintomas[MAX_COMPRIMENTO_SINTOMAS];
+    int gravidade; 
+} Paciente;
+
+void limparTela();
+void pausarTela();
+void adicionarPaciente(Paciente pacientes[], int *quantidadePacientes);
+void triagemPacientes(Paciente pacientes[], int quantidadePacientes);
+void menu();
+void executarOpcao(int opcao, Paciente pacientes[], int *quantidadePacientes);
+
+#endif
